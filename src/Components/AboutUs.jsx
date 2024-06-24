7203e7e8f9af4947168fad6d3faac4c9d1d3bfab
(˃ᆺ˂) <2300031042@kluniversity.in>
2024-06-24 20:03:36 +0530

    changed target to blank

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -16,7 +16,7 @@ function AboutUs() {
     <div className='w-full h-full bg-white flex-col justify-center '>
       <h1 className='headding text-center text-red-500 font-bold font-[oswald] text-4xl  mt-6 tracking-wide'>About Us</h1>
        <div className=' w-full h-40 flex justify-center items-center mt-36'>
-      <a href="https://www.instagram.com/_university_studio_/">  <Instagram size={78} /></a>
+      <a target='_blank' href="https://www.instagram.com/_university_studio_/">  <Instagram size={78} /></a>
        </div>
     </div>
   )
