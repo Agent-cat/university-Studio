d0e8b3c403ae9923657fb13f1a873d5415b1e004
(˃ᆺ˂) <2300031042@kluniversity.in>
2024-07-03 15:27:17 +0530

    gsap

--- src/Components/FirstYear.jsx
+++ src/Components/FirstYear.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FirstYearSubjects } from '../constants/constants'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
+import gsap from 'gsap'
 const FirstYear = () => {
+  useGSAP(() => {
+    gsap.from(".Notes", {
+      opacity: 0,
+      scale: 0.5,
+      x: -100,
+      stagger: 0.2,
+      duration: 1,
+      
+    });
+  });
   return (
     <div className=' w-full h-screen overflow-scroll '>
        <div className=" ml-10 md:ml-7 flex items-center ">
@@ -10,7 +24,7 @@ const FirstYear = () => {
             <ul className=" grid gris-cols-1 md:grid-cols-3 gap-4  items-center mx-[10%] md:mx-0  ">
               {
                 FirstYearSubjects.map((subject,index)=>(
-                  <div key={index} className="  col-span-1  text-black bg-white shadow-md bg-clip-border rounded-xl   w-[90%] md:w-auto">
+                  <div key={index} className=" Notes  col-span-1  text-black bg-white shadow-md bg-clip-border rounded-xl   w-[90%] md:w-auto">
                   <div className="  mx-4 mt-4 overflow-hidden  text-gray-700 bg-white shadow-lg bg-clip-border rounded-xl h-40">
                     <img src="https://docs.material-tailwind.com/img/team-3.jpg" alt="profile-picture" />
                   </div>
