cbaa89e9034316c6cfdb399fb0fd01060ba46385
(˃ᆺ˂) <2300031042@kluniversity.in>
2024-07-07 00:16:52 +0530

    Changed image in notes

--- src/Components/FirstYear.jsx
+++ src/Components/FirstYear.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { FirstYearSubjects } from '../constants/constants'
 import { useGSAP } from '@gsap/react'
-import {CircleAlert} from 'lucide-react';
+import Lace from '../../Images/Lace.png'
+import Book from '../../Images/Books.png'
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 gsap.registerPlugin(ScrollTrigger);
 import gsap from 'gsap'
 const FirstYear = () => {
@@ -17,25 +19,28 @@ const FirstYear = () => {
     });
   });
   return (
-    <div className=' w-full h-screen overflow-scroll '>
+    <div className=' w-full h-screen overflow-scroll overflow-x-hidden '>
        <div className=" ml-10 md:ml-7 flex items-center ">
         
           <div >
+          
             <ul className=" grid gris-cols-1 md:grid-cols-3 gap-4  items-center mx-[10%] md:mx-0  ">
+
               {
                 FirstYearSubjects.map((subject,index)=>(
-                  <div key={index} className="   col-span-1 hover:scale-105 duration-100  text-black bg-white shadow-md bg-clip-border rounded-xl   w-[90%] md:w-auto">
+                  <div key={index} className=" z-50  col-span-1  hover:scale-105 duration-100  text-black bg-white shadow-md bg-clip-border rounded-xl   w-[90%] md:w-auto">
                   <div className="  mx-4 mt-4 overflow-hidden  text-gray-700 bg-white shadow-lg bg-clip-border rounded-xl h-48">
-                    <img src="https://docs.material-tailwind.com/img/team-3.jpg" alt="profile-picture" />
+                    <img src={Lace} />
                   </div>
                   <div className="p-6 text-center">
                     <h4 className="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-tight text-blue-gray-900">
                       {subject.lable}
                     </h4>
                     <p
-                      className="block font-sans text-base antialiased text-red-500 font-medium leading-relaxed  bg-clip-text bg-gradient-to-tr from-blue-gray-600 to-blue-gray-400">
+                      className="block font-sans text-base antialiased text-red-500  font-medium leading-relaxed  bg-clip-text bg-gradient-to-tr from-blue-gray-600 to-blue-gray-400">
                        <span className='font-medium text-lg'>!</span> According to Y-23
                     </p>
+                    
                   </div>
                   <div className="flex justify-center p-6 pt-2 gap-7">
                   <Link target='_blank' to={subject.DownloadLink} className="py-2 px-4 text-white font-bold bg-blue-500 rounded-lg ">
@@ -52,6 +57,7 @@ const FirstYear = () => {
           </div>
        
       </div>
+     
     </div>
   )
 }
